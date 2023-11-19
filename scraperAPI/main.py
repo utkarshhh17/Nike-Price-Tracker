b6da3ad4e426df37d4c8d68b25a836d7c1354ab6
shreyash-01 <singhshreyash0075@gmail.com>
2023-11-19 20:14:57 +0530

    Enabled CORS

--- scraperAPI/main.py
+++ scraperAPI/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from URL import URL
 from scraper import Scraper
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/api/products")
 async def root(url: URL):
     s = Scraper()
