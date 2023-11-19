5cce3fb2c0701e260de3314974c0d8d63ec502a6
shreyash-01 <singhshreyash0075@gmail.com>
2023-11-19 18:53:20 +0530

    Scrape functionality done

--- scraperAPI/main.py
+++ scraperAPI/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
+from URL import URL
+from scraper import Scraper
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/api/products")
+async def root(url: URL):
+    s = Scraper()
+    return s.scrape(url)
