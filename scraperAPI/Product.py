d12509eb1858a102a6462acc6da86b87658ec1bb
shreyash-01 <singhshreyash0075@gmail.com>
2023-11-19 18:00:03 +0530

    Created Product class

---
+++ scraperAPI/Product.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, name: str, price: str, url: str, image_url: str):
+        self.name = name
+        self.price = price
+        self.url = url
+        self.image_url = image_url
+
+    def __str__(self):
+        return f"Name: {self.name}, Price: {self.price}, URL: {self.url}, ImageURL: {self.image_url}"
