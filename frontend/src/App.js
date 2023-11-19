b4dc436583e036467d8d96066026b4852e701ad3
utkarshhh17 <singhutkarsh.1210@gmail.com>
2023-11-19 17:43:29 +0530

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+import MainContent from "./MainContent";
+
+function App() {
+  return (
+    <div className="App">
+    <Nav />
+    <MainContent />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Nav.jsx
@@ -0,0 +1,15 @@
+
+export default function Nav(){
+
+
+
+
+    return (
+    <div className={`z-40 fixed w-full flex flex-wrap justify-between mr-10 font-roboto text-white bg-gray-900 h-[60px]`}>
+        <div className="italic flex text-3xl ml-10 cursor-pointer font-bold items-center"><div>Nykaa</div></div>
+       
+
+
+    </div>
+    )
+}
