a6cfd7896c5fe851cba78772957f51e1014ca2f1
utkarshhh17 <singhutkarsh.1210@gmail.com>
2023-11-22 21:43:05 +0530

    more addition

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Nav from "../Nav";
+import MainContent from "../MainContent";
+function HomePage() {
+    return (
+      <div className="App">
+      <Nav />
+      <MainContent />
+  
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
---
+++ frontend/src/Pages/Products.jsx
@@ -0,0 +1,13 @@
+import Nav from "../Nav";
+function ProductPage() {
+    return (
+      <div className="App">
+      <Nav />
+
+  
+      </div>
+    );
+  }
+  
+  export default ProductPage;
+  
